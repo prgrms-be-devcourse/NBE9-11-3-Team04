203c5d81b110e2b5398790f4174b576317fc9218
CHO BOGANG <bogang12345@gmail.com>
2026-05-15 23:50:17 +0900

    Refactor/#71 auth/member enum kotlin 마이그레이션 (#73)
    * refactor: 회원 권한 enum을 Kotlin으로 전환
    
    * refactor: 회원 상태 enum을 Kotlin으로 전환
    
    * refactor: 인증 제공자 enum을 Kotlin으로 전환

---
+++ back/DevC/src/main/kotlin/com/back/devc/domain/member/member/entity/AuthProvider.kt
@@ -0,0 +1,8 @@
+package com.back.devc.domain.member.member.entity
+
+enum class AuthProvider {
+    LOCAL,
+    GITHUB,
+    KAKAO,
+    GOOGLE
+}
---
+++ back/DevC/src/main/kotlin/com/back/devc/domain/member/member/entity/MemberStatus.kt
@@ -0,0 +1,9 @@
+package com.back.devc.domain.member.member.entity
+
+enum class MemberStatus {
+    ACTIVE,
+    WARNED,
+    SUSPENDED,
+    BLACKLISTED,
+    WITHDRAWN
+}
