a19913a741dbbe42f5b6787adfb0133ca9788e31
bokang03 <bogang12345@gmail.com>
2026-05-15 10:19:04 +0900

    chore: Kotlin 환경 설정 및 애플리케이션 진입점 전환

--- back/DevC/src/main/java/com/back/devc/DevCApplication.kt
+++ back/DevC/src/main/java/com/back/devc/DevCApplication.kt
@@ -1,14 +1,13 @@
 package com.back.devc
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableJpaAuditing
-object DevCApplication {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        SpringApplication.run(DevCApplication::class.java, *args)
-    }
+open class DevCApplication
+
+fun main(args: Array<String>) {
+    runApplication<DevCApplication>(*args)
 }
