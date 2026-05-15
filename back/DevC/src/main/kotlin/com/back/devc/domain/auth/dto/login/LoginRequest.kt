2cac132f1bbce4ae81252aeaa5e8fba917758797
bokang03 <bogang12345@gmail.com>
2026-05-15 20:44:15 +0900

    refactor: 로그인 DTO를 Kotlin으로 전환

---
+++ back/DevC/src/main/kotlin/com/back/devc/domain/auth/dto/login/LoginRequest.kt
@@ -0,0 +1,13 @@
+package com.back.devc.domain.auth.dto.login
+
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+
+data class LoginRequest(
+    @field:NotBlank(message = "이메일을 입력해주세요.")
+    @field:Email(message = "이메일 형식이 올바르지 않습니다.")
+    val email: String,
+
+    @field:NotBlank(message = "비밀번호를 입력해주세요.")
+    val password: String
+)
