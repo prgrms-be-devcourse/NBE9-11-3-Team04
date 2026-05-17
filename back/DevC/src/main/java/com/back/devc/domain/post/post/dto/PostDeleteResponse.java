4221432b28bb8e5b419abf3604f93db70d43689c
RkdJIHYE <kangbs210210@gmail.com>
2026-05-17 17:17:26 +0900

    refactor : PostDeleteResponse 코틀린 전환

--- back/DevC/src/main/java/com/back/devc/domain/post/post/dto/PostDeleteResponse.java
+++
@@ -1,9 +0,0 @@
-package com.back.devc.domain.post.post.dto;
-
-public record PostDeleteResponse(
-        Long postId
-) {
-    public static PostDeleteResponse of(Long postId) {
-        return new PostDeleteResponse(postId);
-    }
-}
