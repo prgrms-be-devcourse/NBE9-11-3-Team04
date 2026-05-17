1fef877a8f7b3e010a3716ebce3bbd03f57d0457
hansol0118 <wall197944@naver.com>
2026-05-18 05:00:53 +0900

    migration:enum 파일 마이그레이션

--- back/DevC/src/main/java/com/back/devc/domain/interaction/report/entity/SanctionType.java
+++
@@ -1,7 +0,0 @@
-package com.back.devc.domain.interaction.report.entity;
-
-public enum SanctionType {
-    WARNED, // 경고
-    SUSPENDED, //기간 정지
-    BLACKLISTED// 차단
-}
---
+++ back/DevC/src/main/kotlin/com/back/devc/domain/interaction/report/entity/TargetType.kt
@@ -0,0 +1,6 @@
+package com.back.devc.domain.interaction.report.entity
+
+enum class TargetType {
+    POST,
+    COMMENT
+}
