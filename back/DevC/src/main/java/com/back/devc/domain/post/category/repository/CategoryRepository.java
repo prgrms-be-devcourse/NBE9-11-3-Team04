c2c010a0a98869b28e94c75bc0a50ee33153354e
RkdJIHYE <kangbs210210@gmail.com>
2026-05-18 16:45:41 +0900

    refactor : category repository 코틀린 전환

--- back/DevC/src/main/java/com/back/devc/domain/post/category/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.back.devc.domain.post.category.repository;
-
-import com.back.devc.domain.post.category.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-
-}
