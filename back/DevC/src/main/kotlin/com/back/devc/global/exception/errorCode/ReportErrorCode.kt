a09c9eb9c37ca02dbe684dae61a1509fc3d6fcf6
hansol0118 <wall197944@naver.com>
2026-05-19 16:29:00 +0900

    feat : ReportGroup 엔터티 추가

--- back/DevC/src/main/kotlin/com/back/devc/global/exception/errorCode/ReportErrorCode.kt
+++ back/DevC/src/main/kotlin/com/back/devc/global/exception/errorCode/ReportErrorCode.kt
@@ -19,7 +19,7 @@ enum class ReportErrorCode(
     REPORT_404_PENDING_LIST(HttpStatus.NOT_FOUND, "REPORT_404", "처리 가능한 대기 상태의 신고가 없습니다."),
 
     REPORT_409_ALREADY_REPORT_USER(HttpStatus.CONFLICT, "REPORT_409", "이미 신고한 대상입니다."),
-    REPORT_409_ALREADY_REPORT(HttpStatus.CONFLICT, "REPORT_409", "이미 처리된 신고입니다."),
+    REPORT_GROUP_409_ALREADY_REPORT(HttpStatus.CONFLICT, "REPORT_409", "이미 처리된 신고입니다."),
 
-    REPORT_410_ALREADY_DELETED(HttpStatus.GONE, "REPORT_410", "이미 삭제된 대상은 신고할 수 없습니다.");
+    REPORT_410_ALREADY_DELETED(HttpStatus.GONE, "REPORT_410", "이미 삭제된 대상은 신고할 수 없습니다."),
 }
